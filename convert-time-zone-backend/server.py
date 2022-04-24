2b12829a14432da5e3680d81daf43dd99751412d
pasan04 <pasankamburugamuwa1@gmail.com>
2022-04-24 23:16:04 +0530

    Change the backend server port

--- convert-time-zone-backend/server.py
+++ convert-time-zone-backend/server.py
@@ -35,4 +35,4 @@ def getCurrentTime():
     return jsonify(convertedDateTime)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=3000, debug=True)
--- convert-time-zone-frontend/src/shared/constant.js
+++ convert-time-zone-frontend/src/shared/constant.js
@@ -1,3 +1,3 @@
 // export const url = 'http://ec2-3-15-4-250.us-east-2.compute.amazonaws.com:8080';
 
-export const url = 'http://localhost:8080';
+export const url = 'http://localhost:3000';
